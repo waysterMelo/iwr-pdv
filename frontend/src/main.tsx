0c6385b5d2918161472a73a61a2ff751b64d5df0
Wayster cruz de melo <57998761+waysterMelo@users.noreply.github.com>
2026-05-28 12:01:24 -0300

    feat: load PDV gold card layer

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './pdv-gold-cards.css'
 import App from './App.tsx'
 import { AppMessageProvider } from './components/AppMessageModal.tsx'
 
