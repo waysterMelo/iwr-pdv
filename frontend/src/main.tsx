f0edc885c382a0936f04f3616c2ca0bafdc15a29
Wayster cruz de melo <57998761+waysterMelo@users.noreply.github.com>
2026-05-29 16:28:04 -0300

    feat: load global typography system

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './global-typography.css'
 import './ui-overhaul.css'
 import './pdv-gold-cards.css'
 import App from './App.tsx'
