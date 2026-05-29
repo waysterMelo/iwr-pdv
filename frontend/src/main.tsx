f54f49783c9d04d2bb9f589389a756084480a0d3
Wayster cruz de melo <57998761+waysterMelo@users.noreply.github.com>
2026-05-29 16:34:15 -0300

    fix: load workspace width normalization

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import './global-typography.css'
 import './ui-overhaul.css'
 import './pdv-gold-cards.css'
+import './global-workspace-width.css'
 import App from './App.tsx'
 import { AppMessageProvider } from './components/AppMessageModal.tsx'
 
